090b36868d24f4fc2986d00962172cae0e7d9071
Bob Belderbos <bobbelderbos@gmail.com>
2023-10-25 12:01:06 +0200

    refactoring into class + adding type hints

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import streamlit as st
 
-from data import update_workout_exercise, get_exercises
+from data import WorkoutTracker
+
+tracker = WorkoutTracker()
 
 st.title("Update Workout")
 
 # Dropdown to select exercise
-exercise_name = st.selectbox("Select an Exercise", get_exercises())
+exercise_name = st.selectbox("Select an Exercise", tracker.get_exercises())
 
 # Input field for the exercise value
 exercise_value = st.text_input("Enter the exercise value (e.g., 50x12 60x10):")
 
 # Button to trigger update
 if st.button("Update Exercise"):
-    if not exercise_value:
+    if exercise_name is None:
+        st.warning("Please select an exercise!")
+    elif not exercise_value:
         st.warning("Please input a value for the exercise!")
     else:
-        update_workout_exercise(exercise_name, exercise_value)
+        tracker.update_workout_exercise(exercise_name, exercise_value)
         st.success(f"Updated {exercise_name} with value: {exercise_value}")
-
--- data.py
+++ data.py
@@ -4,51 +4,56 @@ import os
 
 import gspread
 
-
-GOOGLE_SERVICE_ACCOUNT_JSON = os.environ['GOOGLE_SERVICE_ACCOUNT_JSON']
-creds_dict = json.loads(GOOGLE_SERVICE_ACCOUNT_JSON)
-gc = gspread.service_account_from_dict(creds_dict)
-
-sheet = gc.open("bob-workouts").sheet1
+GOOGLE_SERVICE_ACCOUNT_JSON = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
 
 
-def get_exercises():
-    return sheet.col_values(2)[1:]
+class ExerciseNotFoundError(Exception):
+    """Raised when the specified exercise is not found in the sheet."""
 
 
-def _get_monday_of_week():
-    today = datetime.date.today()
-    days_since_monday = today.weekday()
-    last_monday = today - datetime.timedelta(days=days_since_monday)
-    date_str = last_monday.strftime("%d/%m/%Y")
-    return date_str
+class DateNotFoundError(Exception):
+    """Raised when the specified date is not found in the sheet."""
 
 
-def get_column_to_update():
-    date_str = _get_monday_of_week()
-    row = sheet.row_values(1)
-    if date_str not in row:
-        raise Exception("Date not found in row")
-    return row.index(date_str) + 1
+class WorkoutTracker:
+    def __init__(self, sheet_name: str = "bob-workouts") -> None:
+        creds_dict = json.loads(GOOGLE_SERVICE_ACCOUNT_JSON)
+        gc = gspread.service_account_from_dict(creds_dict)
+        self.sheet = gc.open(sheet_name).sheet1
 
+    def get_exercises(self) -> list[str]:
+        return self.sheet.col_values(2)[1:]
 
-def _get_exercise_row_number(exercise_name):
-    exercises = get_exercises()
-    if exercise_name not in exercises:
-        # TODO: can use own named exception
-        raise Exception("Exercise not found")
-    return exercises.index(exercise_name) + 2  # +1 for header row, +1 for 0 index
+    def _get_monday_of_week(self) -> str:
+        today = datetime.date.today()
+        days_since_monday = today.weekday()
+        last_monday = today - datetime.timedelta(days=days_since_monday)
+        return last_monday.strftime("%d/%m/%Y")
 
+    def get_column_to_update(self) -> int:
+        date_str = self._get_monday_of_week()
+        row = self.sheet.row_values(1)
+        if date_str not in row:
+            raise DateNotFoundError(f"Date {date_str} not found in row")
+        return row.index(date_str) + 1
 
-def update_workout_exercise(exercise_name, exercise_value):
-    try:
-        row = _get_exercise_row_number(exercise_name)
-    except Exception:
-        # TODO: return early ok here?
-        return
-    col = get_column_to_update()
-    sheet.update_cell(row, col, exercise_value)
+    def _get_exercise_row_number(self, exercise_name: str) -> int:
+        exercises = self.get_exercises()
+        if exercise_name not in exercises:
+            raise ExerciseNotFoundError(f"Exercise {exercise_name} not found")
+        return exercises.index(exercise_name) + 2  # +1 for header row, +1 for 0 index
+
+    def update_workout_exercise(self, exercise_name: str, exercise_value: str) -> None:
+        # we assume unique exercises for now
+        try:
+            row = self._get_exercise_row_number(exercise_name)
+        except ExerciseNotFoundError:
+            return None
+        col = self.get_column_to_update()
+        self.sheet.update_cell(row, col, exercise_value)
+        return None
 
 
 if __name__ == "__main__":
-    update_workout_exercise("bicep curl", "20x20x20x20")
+    tracker = WorkoutTracker()
+    tracker.update_workout_exercise("bicep curl", "20x20x20x20")
